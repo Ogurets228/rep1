74cecb1a5d7c3b2e26b815eb2dca4deb013a1724
Sanda <sandu.bolocan.tmrg@gmail.com>
2022-03-02 17:40:42 +0200

    References of pointers of arrays as parameters of functions

--- rep_cpp/rep_cpp.cpp
+++ rep_cpp/rep_cpp.cpp
@@ -1,24 +1,57 @@
 ﻿#include <iostream>
-#include <string>
-#include <ctime>
 
 using namespace std;
 
+void Foo(int* a, int *size)
+{
+	*size = 10;
+
+	delete[] a;
+	a = new int[*size];
+	for (int i = 0; i < *size; i++)
+	{
+		a[i] = i * i;
+	}
+}
+
+int* addElement(int* arr, int* size, int element)
+{
+	int* newArr = new int[*size + 1];
+	for (int i = 0; i < *size; i++)
+	{
+		newArr[i] = arr[i];
+	}
+	newArr[*size] = element;
+	delete[] arr;
+	arr = new int[*size + 1];
+	for (int i = 0; i<*size+1; i++)
+	{
+		arr[i] = newArr[i];
+	}
+	*size += 1;
+
+	return arr;
+}
+
 int main()
 {
-    string hopa[7] = { "Gay string: ", "Oh no! It's ", "Let's read: ", "Prankkkkkkkkk ", "Poopa ", "jdfhjsdcvf", "Reshka " };
+	int *size = new int;
+	*size = 5;
+	int* pArr = new int[*size];
+	pArr[0] = 12;
+	pArr[1] = 15642;
+	pArr[2] = 3412;
+	pArr[3] = 152;
+	pArr[4] = 125;
 
-    int size;
-    cin >> size;
-    string *arr = new string[size];
+	pArr = addElement(pArr, size, 111);
 
-    srand(time(0));
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = hopa[rand()%7] + to_string(rand() % 10 + 1);
-        cout << "Element " << i << ":" << arr[i] << endl;
-    }
+	for (int i = 0; i < *size; i++)
+	{
+		cout << *(pArr + i) << endl;
+	}
+	delete[] pArr;
 
-    delete[] arr;
-    return 0;
+	system("pause");
+	return 0;
 }
