adc9ac90b01cf689c01ee1dc317b219200b85d14
Sanda <sandu.bolocan.tmrg@gmail.com>
2022-03-02 09:12:15 +0200

    One more commit

--- rep_cpp/rep_cpp.cpp
+++ rep_cpp/rep_cpp.cpp
@@ -1,17 +1,24 @@
 ﻿#include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
-void reMake(int* pInt)
-{
-    *pInt = 8888;
-}
-
 int main()
 {
-    int lol = 5;
-    reMake(&lol);
-    cout << lol;
+    string hopa[7] = { "Gay string: ", "Oh no! It's ", "Let's read: ", "Prankkkkkkkkk ", "Poopa ", "jdfhjsdcvf", "Reshka " };
+
+    int size;
+    cin >> size;
+    string *arr = new string[size];
+
+    srand(time(0));
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = hopa[rand()%7] + to_string(rand() % 10 + 1);
+        cout << "Element " << i << ":" << arr[i] << endl;
+    }
+
+    delete[] arr;
     return 0;
 }
