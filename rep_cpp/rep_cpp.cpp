a1a16d2c80d6e854c47daed2be8361666f3745a6
Sanda <sandu.bolocan.tmrg@gmail.com>
2022-03-03 10:10:23 +0200

    Pointers of reference address (Ukazateli na ssilki)

--- rep_cpp/rep_cpp.cpp
+++ rep_cpp/rep_cpp.cpp
@@ -33,25 +33,44 @@ int* addElement(int* arr, int* size, int element)
 	return arr;
 }
 
+void ShowArray(int arr[], int size)
+{
+	cout << "arr's elements: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+}
+
+void FillArray(int arr[], int size, int max=10)
+{
+	srand(time(0));
+	for (int i = 0; i < size; i++)
+	{
+		*(arr + i) = rand() % max + 1;
+	}
+}
+
 int main()
 {
-	int *size = new int;
-	*size = 5;
-	int* pArr = new int[*size];
-	pArr[0] = 12;
-	pArr[1] = 15642;
-	pArr[2] = 3412;
-	pArr[3] = 152;
-	pArr[4] = 125;
-
-	pArr = addElement(pArr, size, 111);
-
-	for (int i = 0; i < *size; i++)
-	{
-		cout << *(pArr + i) << endl;
-	}
+	int b[] = {3, 654, 543, 89};
+	int* pArr = new int[3];
+	pArr[0] = 90;
+    pArr[1] = 95;
+	pArr[2] = 100;
+	ShowArray(pArr, 3);
+	cout << "Changing size of arr...\n";
+	
+	//cout << "array first element's address: " << pArr << endl;
+	//cout << "should be array first element's address: " << *&pArr << endl;
+	int*& pRef = *&pArr;
 	delete[] pArr;
+	pRef = new int[5];
+	FillArray(pArr, 5);
+	ShowArray(pArr, 5);
 
+	delete[] pArr;
 	system("pause");
 	return 0;
 }
