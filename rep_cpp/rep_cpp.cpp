4d073269aa3058c934422451f3cc094c5ae22ebe
Sanda <sandu.bolocan.tmrg@gmail.com>
2022-03-03 13:28:15 +0200

    Progressing...

--- rep_cpp/rep_cpp.cpp
+++ rep_cpp/rep_cpp.cpp
@@ -1,8 +1,8 @@
-﻿#include <iostream>
+﻿/*#include <iostream>
 
 using namespace std;
 
-void Foo(int* a, int *size)
+void Foo(int* a, int* size)
 {
 	*size = 10;
 
@@ -24,7 +24,7 @@ int* addElement(int* arr, int* size, int element)
 	newArr[*size] = element;
 	delete[] arr;
 	arr = new int[*size + 1];
-	for (int i = 0; i<*size+1; i++)
+	for (int i = 0; i < *size + 1; i++)
 	{
 		arr[i] = newArr[i];
 	}
@@ -43,7 +43,7 @@ void ShowArray(int arr[], int size)
 	cout << endl;
 }
 
-void FillArray(int arr[], int size, int max=10)
+void FillArray(int arr[], int size, int max = 10)
 {
 	srand(time(0));
 	for (int i = 0; i < size; i++)
@@ -54,23 +54,92 @@ void FillArray(int arr[], int size, int max=10)
 
 int main()
 {
-	int b[] = {3, 654, 543, 89};
-	int* pArr = new int[3];
-	pArr[0] = 90;
-    pArr[1] = 95;
-	pArr[2] = 100;
-	ShowArray(pArr, 3);
-	cout << "Changing size of arr...\n";
-	
-	//cout << "array first element's address: " << pArr << endl;
-	//cout << "should be array first element's address: " << *&pArr << endl;
-	int*& pRef = *&pArr;
-	delete[] pArr;
-	pRef = new int[5];
-	FillArray(pArr, 5);
-	ShowArray(pArr, 5);
+	int* arr = new int[3];
+	*arr = 1;
+	*(arr + 1) = 2;
+	*(arr + 2) = 3;
+	ShowArray(arr, 3);
 
-	delete[] pArr;
+	int*& h = arr;
+	cout << "h = " << h << endl;
+	cout << "&h = " << &h << endl;
+	cout << "&arr = " << &arr << endl;
+	cout << "arr = " << arr << endl;
+
+
+	delete[] arr;
+
+	system("pause");
+	return 0;
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+string to_string(int n)
+{
+	char buf[40];
+	sprintf_s(buf, "%d", n);
+	return buf;
+}
+
+template <typename T>
+void ShowArray(T arr[], int size)
+{
+	cout << "arr's elements: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << endl;
+}
+
+void FillArray(int arr[], int size, int max = 10)
+{
+	srand(time(0));
+	for (int i = 0; i < size; i++)
+	{
+		*(arr + i) = rand() % max + 1;
+	}
+}
+void FillArray(string arr[], int size, int max = 10)
+{
+	srand(time(0));
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = "Element " + to_string(i);
+	}
+}
+
+void AddElement(string*& arr, int &size, string element)
+{
+	string* new_arr = new string[size+1];
+	for (int i = 0; i < size; i++)
+	{
+		new_arr[i] = arr[i];
+	}
+	new_arr[size] = element;
+
+	delete[] arr;
+	arr = new_arr;
+	size += 1;
+}
+
+int main()
+{
+	int size = 3;
+	string* p = new string[size];
+	FillArray(p, size);
+	ShowArray(p, size);
+
+	cout << "Adding 1 element...\n";
+	AddElement(p, size, "noooo");
+	ShowArray(p, size);
+
+	delete[] p;
+
 	system("pause");
 	return 0;
 }
